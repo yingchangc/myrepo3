fd3f52a05aeabdf697458c294209f2dc5cb6e9c3
Ubuntu <ubuntu@ip-172-31-12-105.us-west-2.compute.internal>
2014-04-07 06:53:06 +0000

    change code

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
